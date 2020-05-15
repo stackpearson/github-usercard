e1e582049e996687e0023ec5aa135455c1f3f3ea
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-05-14 23:38:16 -0600

    card constructed & data fed in, moving on to adding multiple cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,7 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-let apiCall = axios.get('https://api.github.com/users/stackpearson')
+
 
 
 
@@ -50,6 +50,7 @@ const followersArray = [];
 */
 
 function createGitCards(apiData) {
+  //declaring new elements
   const cardContainer = document.createElement('div')
   const cardImg = document.createElement('img')
   const card = document.createElement('div')
@@ -62,30 +63,60 @@ function createGitCards(apiData) {
   const cardFollowingP = document.createElement('p')
   const cardBioP = document.createElement('p')
 
+  //adding classes
   cardContainer.classList.add('card')
   card.classList.add('card-info')
   cardTitle.classList.add('name')
   cardUserP.classList.add('username')
 
-  // loading new elements to the dom
-  cardContainer.appendChild(cardImg)
-  cardContainer.appendChild(card)
-  card.appendChild(cardTitle)
-  card.appendChild(cardUserP)
-  card.appendChild(cardLocationP)
-  card.appendChild(cardProfileP)
-  cardProfileP.appendChild(cardGitAddy)
-  card.appendChild(cardFollowersP)
-  card.appendChild(cardFollowingP)
-  card.appendChild(cardBioP)
-  
+  // adding new elements to the dom
+  cardContainer.append(cardImg, card)
+  card.append(cardTitle, cardUserP, cardLocationP, cardProfileP, cardFollowersP, cardFollowingP, cardBioP)
+  cardProfileP.append(cardGitAddy)
 
   return cardContainer
 }
 
+//adding component to .cards div
 const cardComponent = createGitCards()
 const mainCardHolder = document.querySelector('.cards')
 mainCardHolder.appendChild(cardComponent)
+
+const imgEntry = document.querySelector('.cards img')
+const nameEntry = document.querySelector('.name');
+const usernameEntry = document.querySelector('.username')
+const locationSelector = document.querySelector('p:nth-child(3)')
+// const profileSelector = document.querySelector('p:nth-child(4)')
+const profileSelector = document.querySelector('a::before')
+const linkSelector = document.querySelector('a')
+
+const followersSelector = document.querySelector('p:nth-child(5)')
+const followingSelector = document.querySelector('p:nth-child(6)')
+const bioSelector = document.querySelector('p:nth-child(7)')
+//
+const apiCall = axios.get('https://api.github.com/users/stackpearson')
+
+
+
+.then(response => {
+  console.log(response.data)
+  imgEntry.src = response.data.avatar_url
+  nameEntry.textContent = response.data.name
+  usernameEntry.textContent = response.data.login
+  locationSelector.textContent = 'Location: ' + response.data.location
+  linkSelector.setAttribute('href', response.data.html_url)
+  linkSelector.textContent = 'github link'
+  followersSelector.textContent = 'Followers: ' + response.data.followers
+  followingSelector.textContent = 'Following: ' + response.data.following
+  followingSelector.textContent = 'Following: ' + response.data.following
+  bioSelector.textContent = 'Bio: ' + response.data.bio
+  })
+
+.catch(error => {
+  console.log('Data not returned', error)
+})
+
+
 
 
 /* List of LS Instructors Github username's: 
